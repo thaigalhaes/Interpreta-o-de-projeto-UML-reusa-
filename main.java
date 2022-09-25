2edfc0edb2767f8efa8bbcf6649f4d874b976485
Thainá Magalhães de Sousa <80012076+thaigalhaes@users.noreply.github.com>
2022-09-25 19:40:21 -0300

    Create main java

---
+++ main.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+class main {
+class Funcionario {
+ private static void main (String[] args0 {
+  String nome;
+  } 
+ }
+class RequisicaoTranferencia {
+  private static void main (String[] args {
+    Date dateRequisicao;
+  }
+}
+class Material {   
+  private static void main (String[] args) {
+  String descricao;
+  double preco;
+  }
+  
+}
+}
+                              
+                           
